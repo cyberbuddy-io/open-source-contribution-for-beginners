383dbc82772cede4fc655c3f8b5e0f146721a523
cyberbuddy-manas <rspmanas1@gmail.com>
2021-12-18 15:46:22 +0530

    removing powershell folder

--- Powershell Scripts/hello-world.ps1
+++
@@ -1,6 +0,0 @@
-Clear-Host
-
-Write-Host "Hello World"
-Write-Host "This is a really cool way to work with GitHub"
-Write-Host "Thanks to all the participants for joining us"
-Write-Host "All the participants shall see all the steps carefull so that they can actually learn about GIT and Github"

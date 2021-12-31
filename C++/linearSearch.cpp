79c349709c4ac0ff0f25f459d5ec72d17ef0fe1d
meet-s111 <sukhmeets111@gmail.com>
2021-12-31 13:29:46 +0530

    adding linear search

---
+++ C++/linearSearch.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class LinearSearch{
+    private:
+    int n;
+    int k;
+    int c=1;
+    int arr[];
+
+public:
+    LinearSearch(){
+    cout << "Enter the size of array ->  ";
+    cin >> n;
+    arr[n];
+    for (int i = 0; i < n;i++){
+        cout << "Add the "<<"["<<c<<"]"<<" element of array - >  ";
+        cin >>arr [i];
+        if(arr[i]=='Q'||arr[i]=='q'){
+            break;
+        }
+        c++;
+    }
+    }
+    void search(){
+        cout << "Enter the element which you want to find - >  " ;
+        cin >> k;
+        for (int i = 0; i <= n;i++){
+            if(arr[i]==k){
+                cout << "<----------     Element " << arr[i] << " found at position->  " << i+1<<"     ---------->"<<endl;
+                break;
+            }
+            else  {
+                    if(i==n){
+                        
+                    cout << "<----------     Element not present     ---------->" << endl;
+                    break;
+                    }
+                }
+            }
+        }
+};
+
+int main(){
+    LinearSearch ls;
+    ls.search();
+    return 0;
+}

a9980b2d9b75c80598b22b852ac63edfa7220e04
Anhat Singh <anhatsingh2001@gmail.com>
2022-01-17 08:29:52 +0530

    Stacks and Queues using Python

---
+++ Python/Data Structures/queue.py
@@ -0,0 +1,51 @@
+from typing import Any
+from xmlrpc.client import Boolean
+
+
+class Queue:
+    def __init__(self) -> None:
+        """creates a queue data structure using linear array
+
+        Methods: enqueue, dequeue, isEmpty
+        """
+        self.q = []
+    
+    def __str__(self) -> str:
+        """Used for printing the entire queue.
+
+        Returns:
+            str: Entire Queue as string to be printed out
+        """
+        return str(self.q)
+
+    def enqueue(self, element) -> None:
+        """Add an object at the end of the Queue
+
+        Args:
+            element (<Any>): Object to be added to the Queue
+        """
+        self.q.append(element)
+    
+    def dequeue(self) -> Any:
+        """Remove and return an element from the front of the Queue
+
+        Raises:
+            Exception: Queue Underflow: Raised if the Queue is empty and nothing to return
+
+        Returns:
+            Any: Object at the front of the Queue
+        """
+        if not self.isEmpty():
+            front = self.q[0]
+            self.q = self.q[1:]
+            return front
+        else:
+            raise Exception("Queue Underflow")
+    
+    def isEmpty(self) -> Boolean:
+        """Check if the Queue is Empty
+
+        Returns:
+            Boolean: True if Empty, False otherwise.
+        """
+        return True if self.q == [] else False

48d1b94b7fda6fe6463759e75f97ea1bc5fbbaca
Anhat Singh <anhatsingh2001@gmail.com>
2022-01-18 04:11:00 +0530

    Changed the position of Python Data Structures place to Data Structures Folder. Added Cpp Data Structures in its respective folder

---
+++ Data structure/Python/graph.py
@@ -0,0 +1,7 @@
+from typing import List, Tuple
+import numpy as np
+
+class graph:
+    def __init__(self, node_list:List[Tuple[int, int]], edge_list: list) -> None:
+        #coming soon
+        pass

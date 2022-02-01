f5094914b8e9fe1f8d7264123f8d755e70402706
sukhmeet singh <sukhmeets111@gmail.com>
2022-02-01 13:24:40 +0530

    adding exercise files

---
+++ Java/Ex9.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Ex9 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        String st=s.nextLine();
+        System.out.println("Your Entered string in lower character is \""+st.toLowerCase()+"\"");
+        s.close();
+    }
+}

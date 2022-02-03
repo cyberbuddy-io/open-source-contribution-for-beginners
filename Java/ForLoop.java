9afdf35af7465a169a92d1288cf25b458767d46e
sukhmeet singh <sukhmeets111@gmail.com>
2022-02-03 21:38:01 +0530

    adding loops

---
+++ Java/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        int a=0;
+        for(a=0;a<5;a++){// for loops for certain number of loops provided in the for loop syntax as a condition
+            //in this for loop a=0 is going to repeat until it is a<5 and a++ in for loop keeps incrementing a's value after every loop
+            //first it runs for a=0 then for a=1 then a=2 and so on until a<5 condition is met
+            System.out.println(a);
+            
+        }
+    }
+}

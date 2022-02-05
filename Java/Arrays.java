8042c1569ad85789f5ed762286d2ad398b687df2
sukhmeet singh <sukhmeets111@gmail.com>
2022-02-05 17:40:47 +0530

    adding arrays

--- Java/Arrays.java
+++ Java/Arrays.java
@@ -5,13 +5,13 @@ public class Arrays {
         //1. declaration and memory allocation--> int [] arr=new int[5];
         //2. declaration and then memory allocation--> int [] arr; and then next line arr=new int[5];
         //3.declaration memory allocation and initialization together-->int [] arr={98,45,79};
+
+
+
         //intitialization
         arr[0]=5;
         arr[1]=9;
         System.out.print(arr[1]);
-        // for(int i=0;i<2;i++){
-        //     System.out.print(arr[i]+" ");
-        // }
-        // System.out.print(arr[3]+" ");//it will through error as we have given array the size of 2 only
+        // System.out.print(arr[3]);//it will throw error as we have given array the size of 2 only
     }
 }

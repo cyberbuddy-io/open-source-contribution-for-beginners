35593263f7dd2c142fc8f869852d17e0ce7feb93
Lovish Prabhakar <2020010006574@gndu.ac.in>
2022-04-02 15:12:58 +0530

    Rearranging folders

--- Data structure/Python/graph.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Tuple
-import numpy as np
-
-class graph:
-    def __init__(self, node_list:List[Tuple[int, int]], edge_list: list) -> None:
-        #coming soon
-        pass

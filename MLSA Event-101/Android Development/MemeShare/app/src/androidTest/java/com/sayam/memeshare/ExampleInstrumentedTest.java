a1ff974aec1a7bc7d2e3122849bbe04862d286af
Sayam shama <ssayam200@gmail.com>
2022-04-03 17:24:40 +0530

    MemeShareApp #28

--- MLSA Event-101/Android Development/MemeShare/app/src/androidTest/java/com/sayam/memeshare/ExampleInstrumentedTest.java
+++ MLSA Event-101/Android Development/MemeShare/app/src/androidTest/java/com/sayam/memeshare/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.sayam.recyclerviewbairminimumapp;
+package com.sayam.memeshare;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.sayam.recyclerviewbairminimumapp", appContext.getPackageName());
+        assertEquals("com.sayam.memeshare", appContext.getPackageName());
     }
 }

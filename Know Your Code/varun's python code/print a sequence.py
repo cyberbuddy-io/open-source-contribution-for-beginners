d4d1e5bdf95ca3853f62d932608e5e6df27bbbfb
shardavarun2003 <shardavarun2003@gmail.com>
2022-04-14 09:40:45 +0530

    Signed-off-by: shardavarun2003 <shardavarun2003@gmail.com>

--- Know Your Code/varun's python code/print a sequence.py
+++ Know Your Code/varun's python code/print a sequence.py
@@ -1,4 +1,4 @@
-#Program to print the sequence 1,-4,7,-10,....,-40
+# the Program to print the sequence 1,-4,7,-10,....,-40
 a=input("Press enter to print the required sequence")
 for i in range(1,15):
     j=((-1)**(i+1))*(1+(i-1)*3)

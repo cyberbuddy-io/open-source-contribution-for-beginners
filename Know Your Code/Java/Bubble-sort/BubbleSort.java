d60df33a53556be41b8bfbb6e15b1d5280cd554b
Gustavo Junkes <gujunkes50@gmail.com>
2022-10-07 11:50:55 -0300

    Adding explanation comments to the class.

--- Know Your Code/Java/Bubble-sort/BubbleSort.java
+++ Know Your Code/Java/Bubble-sort/BubbleSort.java
@@ -4,6 +4,7 @@ public class BubbleSort {
         int[] array = {8,6,4,3,8,6,5,8,9,15,2};
         sort(array);
 
+        // just prints the result
         for (int value: array) {
             System.out.print(value + " - ");
         }
@@ -14,6 +15,7 @@ public class BubbleSort {
      */
     public static int[] sort(int[] array) {
         int aux;
+        // iterate the array and verify for each element if is greater than, if yes, change their positions
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if(array[i] < array[j]) {

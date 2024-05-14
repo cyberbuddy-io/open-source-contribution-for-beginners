9c5ddc52d27f93a82efd35add101bf066ef7b8cb
Ram Naresh Kushwah <ramnareshkushwah823@gmail.com>
2024-05-14 11:48:29 +0530

    added cpp patters

---
+++ Know Your Code/C++/Patterns/Pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; ++i)
+    {
+        for (int j = 1; j <= rows - i; ++j)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; ++k)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
